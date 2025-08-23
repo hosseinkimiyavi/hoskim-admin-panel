ae609d52d5289c7972106fcf3f0f30b225f7693c
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2025-08-23 23:26:16 +0330

    add style and colors and dark and light theme, create Context for theme and write the TopBar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-
+import { ThemeProvider } from "@mui/material/styles";
+import { ColorModeContext, useMode } from "./Theme/theme";
+import CssBaseline from "@mui/material/CssBaseline";
+import TopBar from "./scenes/global/TopBar"
 
 function App() {
- 
-
+  const [theme, colorMode] = useMode();
   return (
     <>
-     <div className="app">
-     <h3>hossein</h3>
-     </div>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <main className="content" >
+              <TopBar />
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
